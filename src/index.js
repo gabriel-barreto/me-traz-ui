24c87120ce470a61611a331f413b9360d8f2005f
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-05 22:10:11 -0300

    feat: add mirage server to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { makeServer } from './mirage';
+
 import App from './App';
 
+import { app } from './config';
+
 import * as serviceWorker from './serviceWorker';
+
+if (app.env !== 'production') {
+  makeServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
