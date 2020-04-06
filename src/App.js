7f2e351e22c1aef912f82b666179f735538b4181
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-06 06:10:19 -0300

    feat(root): wrap app component with AppContextProvider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
+
+import AppContextProvider from './contexts';
 import { GlobalStyle, Theme } from './styles';
 
 import Router from './router';
 
 function App() {
   return (
-    <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <div className="App">
-        <Router />
-      </div>
-    </ThemeProvider>
+    <AppContextProvider>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <div className="App">
+          <Router />
+        </div>
+      </ThemeProvider>
+    </AppContextProvider>
   );
 }
 
