53e4ed571f3a4272905a0d84e26426a4d86f5e8d
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-08 07:33:38 -0300

    refactor(product factory): add new props
    - Add UUID as _id
    - Add array of tags

--- src/stories/utils/Factories/product.factories.js
+++ src/stories/utils/Factories/product.factories.js
@@ -3,14 +3,18 @@ import faker from 'faker';
 export const externalPhoto =
   'https://www.maniadechurrasco.com.br/wp-content/uploads/2018/06/PRIME-STEAK-HOUSE-CARDAPIO-02.jpg';
 export const single = (
-  { title, description, price, url } = {
-    title: faker.lorem.words(2),
+  { _id, description, price, tags, title, url } = {
+    _id: faker.random.uuid(),
     description: faker.lorem.paragraph(),
     price: faker.random
       .number({ min: 20, max: 50, precision: 0.99 })
       .toFixed(2),
+    tags: Array.from({ length: faker.random.number({ min: 1, max: 3 }) }, () =>
+      faker.lorem.word(),
+    ),
+    title: faker.lorem.words(2),
     url: faker.random.boolean() ? externalPhoto : null,
   },
-) => ({ title, description, price, photo: { url } });
+) => ({ _id, title, description, price, photo: { url }, tags });
 
 export const list = (seeds = 10) => Array.from({ length: seeds }, single);
