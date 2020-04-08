2c74ee9b77514bd748d7a1fcabe982c9953a0f0d
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-08 12:43:22 -0300

    fix(mirage-factories): fix factories props to not repeat

--- src/mirage/factories/product.js
+++ src/mirage/factories/product.js
@@ -3,31 +3,51 @@ import { Factory } from 'miragejs';
 
 import { randomNumber } from './utils';
 
+const url =
+  'https://img.elo7.com.br/product/zoom/26849BA/adesivo-parede-feijoada-prato-feito-comida-painel-adesivo-feijoada.jpg';
+
 export default Factory.extend({
-  _id: faker.random.uuid(),
-  active: Boolean(randomNumber({ min: 0, max: 1 })),
-  title: faker.lorem.words(randomNumber({ min: 1, max: 3 })),
-  description: faker.lorem.paragraph(1),
-  tags: Array.from({ length: randomNumber({ min: 1, max: 5 }) }, () =>
-    faker.lorem.word(),
-  ),
-  photo: {
-    url:
-      'https://img.elo7.com.br/product/zoom/26849BA/adesivo-parede-feijoada-prato-feito-comida-painel-adesivo-feijoada.jpg',
+  _id() {
+    return faker.random.uuid();
   },
-  additional: Array.from({ length: randomNumber({ min: 0, max: 6 }) }, () => ({
-    label: faker.lorem.words(randomNumber({ min: 1, max: 3 })),
-    price: randomNumber({ min: 1, max: 6 }),
-  })),
-  ingredients: Array.from({ length: randomNumber({ min: 2, max: 6 }) }, () => ({
-    label: faker.lorem.words(randomNumber({ min: 1, max: 3 })),
-    required: Boolean(randomNumber({ min: 0, max: 1 })),
-  })),
+  active: true,
+  title() {
+    return faker.lorem.words(randomNumber({ min: 1, max: 3 }));
+  },
+  description() {
+    return faker.lorem.paragraph(1);
+  },
+  tags() {
+    const length = randomNumber({ min: 1, max: 5 });
+    const mapFn = () => faker.lorem.word();
+    return Array.from({ length }, mapFn);
+  },
+  photo() {
+    return faker.random.boolean() ? { url } : {};
+  },
+  additional() {
+    const length = randomNumber({ min: 0, max: 6 });
+    const mapFn = () => ({
+      label: faker.lorem.words(randomNumber({ min: 1, max: 3 })),
+      price: faker.random
+        .number({ min: 1, max: 6, precision: 0.99 })
+        .toFixed(2),
+    });
+
+    return Array.from({ length }, mapFn);
+  },
+  ingredients() {
+    const length = randomNumber({ min: 2, max: 6 });
+    const mapFn = () => ({
+      label: faker.lorem.words(randomNumber({ min: 1, max: 3 })),
+      required: Boolean(randomNumber({ min: 0, max: 1 })),
+    });
+
+    return Array.from({ length }, mapFn);
+  },
   price() {
-    const price = randomNumber({ min: 19, max: 50 });
-    return {
-      raw: price,
-      formatted: `R$ ${`${price}`.replace('.', ',')}`,
-    };
+    return faker.random
+      .number({ min: 19, max: 50, precision: 0.99 })
+      .toFixed(2);
   },
 });
