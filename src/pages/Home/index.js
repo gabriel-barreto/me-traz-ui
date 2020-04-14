65573fe57d71dd160f6ffe13301a182bb0d0e0b2
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-14 09:00:28 -0300

    feat(home-page): add products context to filter list using search term

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import { $product } from '../../services';
 
+import { useProductsContext } from '../../contexts';
 import { Layout, ProductsGroup } from '../../components';
 
 function HomePage() {
-  const [products, setProducts] = useState([]);
+  const { products, setProducts, search } = useProductsContext();
   useEffect(() => {
     $product
       .fetchAll()
       .then((response) => setProducts(response))
-      .catch((err) => {
+      .catch((_) => {
         // TODO: show a message to user
-        console.log('fetch error');
-        console.log(err);
+        // console.log('fetch error');
+        // console.log(err);
       });
   }, []);
 
+  const list = products.filter((each) => {
+    if (search) {
+      const regex = new RegExp(search, 'i');
+      return regex.test(each.title);
+    }
+    return true;
+  });
+
   return (
     <Layout>
-      <ProductsGroup title="Todos" products={products} />
+      <ProductsGroup title="Todos" products={list} />
     </Layout>
   );
 }
