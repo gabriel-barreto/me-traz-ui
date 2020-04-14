c855737753bd2fa986b9dcd0e2998ddf88aed1f1
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-14 06:02:28 -0300

    feat(navbar): finish responsivity adjustments

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import brand from '../../assets/brand-sm.png';
+
+import SearchForm from '../SearchForm';
+import { Close, Search, Whatsapp } from '../../styles/icons.styles';
 
 import * as S from './styled';
 
 function Navbar() {
+  const [state, setState] = useState({ searchFormVisible: false });
+
+  function toggleSearchForm() {
+    const searchFormVisible = !state.searchFormVisible;
+    setState((prev) => ({ ...prev, searchFormVisible }));
+  }
+
+  const searchTogglerTitle = state.searchFormVisible
+    ? 'Pesquisar Items'
+    : 'Fechar pesquisa';
+
   return (
     <S.Navbar>
       <S.NavBrand src={brand} tilte="Me Traz" alt="Me Traz" />
-      <S.NavSearchForm />
-      <S.NavChatLink href="#">Entrar em Contato</S.NavChatLink>
+
+      <S.NavSearchFormContainer
+        className={`${state.searchFormVisible ? '--visible' : ''}`}
+      >
+        <SearchForm />
+      </S.NavSearchFormContainer>
+
+      <S.NavActions>
+        <S.NavSearchFormToggler
+          onClick={toggleSearchForm}
+          alt={searchTogglerTitle}
+          title={searchTogglerTitle}
+        >
+          {state.searchFormVisible ? <Close size={24} /> : <Search size={24} />}
+        </S.NavSearchFormToggler>
+        <S.NavChatLink
+          href="#"
+          alt="Entrar em Contato pelo WhatsApp"
+          title="Entrar em Contato pelo WhatsApp"
+        >
+          <S.NavChatLinkIcon>
+            <Whatsapp size={24} />
+          </S.NavChatLinkIcon>
+          <S.NavChatLinkLabel>Entrar em Contato</S.NavChatLinkLabel>
+        </S.NavChatLink>
+      </S.NavActions>
     </S.Navbar>
   );
 }
--- src/components/Navbar/styled.js
+++ src/components/Navbar/styled.js
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { breakpoint } from '../../styles';
 
-import SearchForm from '../SearchForm';
+const iconSize = '2.4rem';
+const navbarBorderRadius = '48px';
 
 export const NavBrand = styled.img`
   max-height: 100%;
@@ -11,15 +12,42 @@ export const NavBrand = styled.img`
   width: auto;
 `;
 
-export const NavSearchForm = styled(SearchForm)`
-  display: none;
-  margin: 0 4rem;
+const NavSearchFormVisible = css`
+  opacity: 1;
+  transform: translateY(0);
+  visibility: visible;
+`;
+export const NavSearchFormContainer = styled.div`
+  background-color: ${({ theme }) => theme.hexColors.lightest};
+  box-shadow: 0 2px 0 ${({ theme }) => theme.chromaColors.darkest.alpha(0.24)};
+  display: block;
+  left: 0;
+  opacity: 1;
+  padding: 4.8rem 3.2rem 1.6rem 3.2rem;
+  position: fixed;
+  top: 56px;
+  transform: translateY(-100px);
+  transition: opacity 200ms, transform 400ms, visibility 200ms;
+  visibility: hidden;
+  z-index: -1;
+  width: 100%;
+  will-change: opacity, transform, visibility;
+
   @media screen and (min-width: ${breakpoint('sm')}) {
-    display: block;
+    box-shadow: none;
+    left: auto;
+    padding: 0 4rem;
+    position: relative;
+    top: auto;
+
+    ${NavSearchFormVisible};
+  }
+
+  &.--visible {
+    ${NavSearchFormVisible};
   }
 `;
 
-const navbarBorderRadius = '48px';
 export const Navbar = styled.nav`
   align-items: center;
   background-color: ${({ theme }) => theme.hexColors.lightest};
@@ -29,11 +57,11 @@ export const Navbar = styled.nav`
   height: 10rem;
   justify-content: space-between;
   left: 0;
-  padding: 24px 32px;
+  padding: 2.4rem 3.2rem;
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: 9;
+  z-index: 99;
 
   @media screen and (min-width: ${breakpoint('md')}) {
     padding: 2.4rem 6.4rem;
@@ -44,14 +72,72 @@ export const Navbar = styled.nav`
   }
 `;
 
-export const NavChatLink = styled.a`
+export const NavActions = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const NavSearchFormToggler = styled.button`
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  color: ${({ theme }) => theme.hexColors.primary};
+  display: flex;
+  fill: ${({ theme }) => theme.hexColors.primary};
+  justify-content: center;
+  outline-color: ${({ theme }) => theme.hexColors.primaryDark};
+  padding: 1rem;
+  transition: color 400ms, fill 400ms;
+  will-change: color, fill;
+
+  @media screen and (min-width: ${breakpoint('sm')}) {
+    display: none;
+  }
+`;
+
+export const NavChatLinkIcon = styled.span`
+  color: ${({ theme }) => theme.hexColors.primary};
+  display: block;
+  fill: ${({ theme }) => theme.hexColors.primary};
+  height: ${iconSize};
+  transition: color 400ms, fill 400ms;
+  width: ${iconSize};
+  will-change: color, fill;
+`;
+
+export const NavChatLinkLabel = styled.p`
   color: ${({ theme }) => theme.hexColors.primary};
   font-size: 1.6rem;
   transition: color 400ms;
   white-space: nowrap;
   will-change: color;
+`;
+
+export const NavChatLink = styled.a`
+  align-items: center;
+  display: flex;
+
+  @media screen and (max-width: ${breakpoint('sm')}) {
+    padding: 1rem;
+    > ${NavChatLinkLabel} {
+      display: none;
+    }
+  }
+  @media screen and (min-width: ${breakpoint('sm')}) {
+    padding: 0.8rem;
+    > ${NavChatLinkLabel} {
+      display: initial;
+    }
+    > ${NavChatLinkIcon} {
+      display: none;
+    }
+  }
 
   &:hover {
-    color: ${({ theme }) => theme.hexColors.primaryDark};
+    > ${NavChatLinkLabel}, > ${NavChatLinkIcon} {
+      color: ${({ theme }) => theme.hexColors.primaryDark};
+      fill: ${({ theme }) => theme.hexColors.primaryDark};
+    }
   }
 `;
