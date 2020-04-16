56e19fe357b8fe337ee5d4687ad7da8f8f6c3eba
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-16 07:49:25 -0300

    feat(icons): add check to icons barrel

--- src/styles/icons.styles.js
+++ src/styles/icons.styles.js
@@ -1,3 +1,4 @@
+export { Check } from 'styled-icons/boxicons-regular';
 export { Search } from 'styled-icons/material';
 export { Close } from 'styled-icons/material-rounded';
 export { Whatsapp } from 'styled-icons/remix-line';
