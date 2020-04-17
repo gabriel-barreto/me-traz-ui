71cbe632ef1e215112ec3560c45713fe801418f7
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-17 09:51:01 -0300

    feat(app): add ToastContainer to App root elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -16,6 +18,7 @@ function App() {
           <Router />
         </div>
       </ThemeProvider>
+      <ToastContainer position={toast.POSITION.BOTTOM_CENTER} />
     </AppContextProvider>
   );
 }
