24a4b653ce3b719ef273ec4f34b7ccf76bb9fa67
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-17 10:23:15 -0300

    feat(cart-context): show toast when cart items changes

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,12 +1,22 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 
-const INITIAL_STATE = { items: [], total: 0 };
+const INITIAL_STATE = { items: [], total: 0, pure: true };
 
 const CartContext = createContext(INITIAL_STATE);
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState({ ...INITIAL_STATE });
+  const [cart, rawSetCart] = useState({ ...INITIAL_STATE });
+
+  function setCart(action) {
+    rawSetCart((prev) => ({ ...prev, pure: false }));
+    return rawSetCart(action);
+  }
+
+  useEffect(() => {
+    if (!cart.pure) toast.success('Carrinho Atualizado!');
+  }, [cart]);
 
   return (
     <CartContext.Provider value={{ cart, setCart }}>
