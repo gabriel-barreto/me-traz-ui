100a89245ff9339bb49ec1b78f004412fa025fed
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-17 07:49:10 -0300

    feat(add-to-cart-form): receive and call events by the correct mode

--- src/components/ProductModal/AddToCardForm/index.js
+++ src/components/ProductModal/AddToCardForm/index.js
@@ -3,29 +3,48 @@ import PropTypes from 'prop-types';
 
 import * as S from './styled';
 
-function AddToCartForm({ onQttChange, total }) {
+function AddToCartForm({
+  onAddToCart,
+  onQttChange,
+  onUpdateEntry,
+  total,
+  updateMode,
+}) {
+  function onSubmitHandler(e) {
+    e.preventDefault();
+
+    if (updateMode) return onUpdateEntry();
+    return onAddToCart();
+  }
+
   return (
-    <S.AddToCartForm method="POST" onSubmit={(e) => e.preventDefault()}>
+    <S.AddToCartForm method="POST" onSubmit={onSubmitHandler}>
       <S.AddToCartQttField>
         <S.QttFieldLabel>Quantidade:</S.QttFieldLabel>
         <S.QttFieldInput min={1} max={99} onChange={onQttChange} />
       </S.AddToCartQttField>
-      <S.AddToCartButton>
+      <S.AddToCartButton type="submit">
         <S.AddToCartTotal>
           <S.AddToCardTotalCurrency>R$</S.AddToCardTotalCurrency>
           <S.AddToCardTotalValue>
             {`${parseFloat(total).toFixed(2)}`.replace('.', ',')}
           </S.AddToCardTotalValue>
         </S.AddToCartTotal>
-        <S.AddToCartLabel>põe na sacola!</S.AddToCartLabel>
+        <S.AddToCartLabel>
+          {updateMode ? 'Atualizar' : 'põe na sacola'}
+        </S.AddToCartLabel>
       </S.AddToCartButton>
     </S.AddToCartForm>
   );
 }
 
+AddToCartForm.defaultProps = { updateMode: false };
 AddToCartForm.propTypes = {
+  onAddToCart: PropTypes.func.isRequired,
   onQttChange: PropTypes.func.isRequired,
+  onUpdateEntry: PropTypes.func.isRequired,
   total: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  updateMode: PropTypes.bool,
 };
 
 export default AddToCartForm;
