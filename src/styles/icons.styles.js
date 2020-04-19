e796f3de3fafc94b07e85f8cc5c1e703c3b24857
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-19 00:34:05 -0300

    feat(icons-barrel): export ShoppingBag from boxicons

--- src/styles/icons.styles.js
+++ src/styles/icons.styles.js
@@ -1,4 +1,5 @@
 export { Check } from 'styled-icons/boxicons-regular';
+export { ShoppingBag } from 'styled-icons/boxicons-solid';
 export { Search } from 'styled-icons/material';
 export { Close } from 'styled-icons/material-rounded';
 export { Whatsapp } from 'styled-icons/remix-line';
