eac6c4a44b2cbdaff21a919de8fbd7174bf5ccf0
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-19 06:15:18 -0300

    feat(view-cart-button): show cart qtt
    - Use counter element to show cart items
    - Get cart items using context
    - Reduce elements to consider qtt of each product
    - Use a function to produce a fancy label to quantity

--- src/components/ViewCartButton/index.js
+++ src/components/ViewCartButton/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import { useCart } from '../../contexts';
 import { ShoppingBag } from '../../styles/icons.styles';
 
 import * as S from './styled';
 
 function ViewCartButton() {
+  const { cart } = useCart();
+  const qtt = (() => cart.items.reduce((prev, curr) => prev + curr.qtt, 0))();
+  const fancyQtt = (() => (qtt > 9 ? '9+' : qtt))();
+
   return (
-    <S.ViewCartButtonWrapper>
+    <S.ViewCartButtonWrapper className={qtt > 0 ? '--visible' : ''}>
+      <S.ViewCartButtonCounter>{fancyQtt}</S.ViewCartButtonCounter>
       <S.ViewCartButtonIcon>
         <ShoppingBag size={24} />
       </S.ViewCartButtonIcon>
