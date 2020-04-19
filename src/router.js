fad48078941b5a7333f3a264a6f1fde31acf787b
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-19 08:10:02 -0300

    feat(router): add checkout page to router

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 
-import { Home } from './pages';
+import { Checkout, Home } from './pages';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Route component={Checkout} path="/sacola" />
       <Route component={Home} path="/" exact />
-      <Redirect to={'/'} />
+      <Redirect to="/" />
     </BrowserRouter>
   );
 }
