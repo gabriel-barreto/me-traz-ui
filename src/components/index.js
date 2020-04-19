727e71cafa29bb818295298500bcec74292616ac
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-19 05:18:07 -0300

    feat(components-barrel): export ViewCartButton

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Layout } from './Layout';
 export { default as ProductsGroup } from './ProductsGroup';
 export { default as ProductModal } from './ProductModal';
+export { default as ViewCartButton } from './ViewCartButton';
