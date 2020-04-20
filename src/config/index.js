13a4add7e9a58b79bc138c08469ef92653f75f68
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-20 12:25:09 -0300

    feat(config): create and export storage config
    - Register an object to hold all sessionStorage keys of app

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,3 @@
 export { default as app } from './app.config';
 export { default as api } from './api.config';
+export { default as storage } from './storage.config';
---
+++ src/config/storage.config.js
@@ -0,0 +1,5 @@
+export default {
+  keys: {
+    cart: 'metraz@cart',
+  },
+};
