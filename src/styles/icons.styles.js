2c231189fd147ae7c71f2c3c98d4433f6cc723bb
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-21 07:19:44 -0300

    feat(icons-barrel): export edit and trash icons

--- src/styles/icons.styles.js
+++ src/styles/icons.styles.js
@@ -1,5 +1,5 @@
 export { Check } from 'styled-icons/boxicons-regular';
-export { ShoppingBag } from 'styled-icons/boxicons-solid';
-export { ChevronLeft, Search } from 'styled-icons/material';
+export { ShoppingBag, Trash } from 'styled-icons/boxicons-solid';
+export { ChevronLeft, Edit, Search } from 'styled-icons/material';
 export { Close } from 'styled-icons/material-rounded';
 export { Whatsapp } from 'styled-icons/remix-line';
