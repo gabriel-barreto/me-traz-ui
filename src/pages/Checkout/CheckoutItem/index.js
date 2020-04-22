e71e96f9076d1c6d0275c2bba9389947ae3e42a6
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-22 07:33:28 -0300

    feat(checkout-item): add and user onEdit and onRemove events
    - Receive onEdit and onRemove funcs on props
    - Pass to CheckoutItem actions
    - Add new props to validation

--- src/pages/Checkout/CheckoutItem/index.js
+++ src/pages/Checkout/CheckoutItem/index.js
@@ -9,7 +9,14 @@ import Ingredients from '../Ingredients';
 
 import * as S from './styled';
 
-function CheckoutItem({ addedAdditional, ingredients, title, price }) {
+function CheckoutItem({
+  addedAdditional,
+  ingredients,
+  onEdit,
+  onRemove,
+  title,
+  price,
+}) {
   const fancyPrice = $PriceFormatter.fancy(price);
 
   return (
@@ -24,10 +31,10 @@ function CheckoutItem({ addedAdditional, ingredients, title, price }) {
       <Ingredients ingredients={ingredients} />
       <Additional additional={addedAdditional} />
       <S.CheckoutItemActionsContainer>
-        <S.CheckoutItemAction type="button">
+        <S.CheckoutItemAction type="button" onClick={onEdit}>
           <Edit size={18} />
         </S.CheckoutItemAction>
-        <S.CheckoutItemAction type="button">
+        <S.CheckoutItemAction type="button" onClick={onRemove}>
           <Trash size={18} />
         </S.CheckoutItemAction>
       </S.CheckoutItemActionsContainer>
@@ -40,6 +47,8 @@ CheckoutItem.propTypes = {
   addedAdditional: Additional.propTypes.additional,
   ingredients: Ingredients.propTypes.ingredients,
   title: PropTypes.string.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  onRemove: PropTypes.func.isRequired,
   price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
 
