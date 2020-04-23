368029200895a3ea149c66a6a9d9cf62a1a16cf2
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-23 02:36:41 -0300

    fix(global-style): google fonts import

--- src/styles/resets.styles.js
+++ src/styles/resets.styles.js
@@ -1,8 +1,6 @@
 import { css } from 'styled-components';
 
 export default css`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
-
   html,
   body,
   div,
