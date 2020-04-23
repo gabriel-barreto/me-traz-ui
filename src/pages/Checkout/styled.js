7c3d4d74e63688d2606f3812ea36d1cbb364f0f3
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-23 00:58:18 -0300

    style(checkout-page): limit cart items width on bigger screens

--- src/pages/Checkout/styled.js
+++ src/pages/Checkout/styled.js
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const CheckoutItemsList = styled.ul``;
+import { breakpoint } from '../../styles';
+
+export const CheckoutItemsList = styled.ul`
+  @media screen and (min-width: ${breakpoint('md')}) {
+    margin: 1.6rem 0 0 auto;
+    max-width: 40rem;
+  }
+`;
