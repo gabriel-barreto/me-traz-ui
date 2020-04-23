693714e87ca90248e333d08d6089be8dde60c982
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-23 03:14:49 -0300

    feat(checkout-page): wrap cart items, add a title to them

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Layout } from '../../components';
@@ -8,9 +8,29 @@ import CheckoutItem from './CheckoutItem';
 import EmptyCheckoutMessage from './EmptyMessage';
 import * as S from './styled';
 
+const INITIAL_STATE = { index: -1, payload: {} };
+
 function CheckoutPage() {
-  const { cart } = useCart();
+  const [, setEdit] = useState({ ...INITIAL_STATE });
+  const { cart, setCart } = useCart();
   const { action, push } = useHistory();
+
+  function onItemEditHandler(itemId) {
+    const index = cart.items.findIndex((each) => each._id === itemId);
+    const payload = cart.items[index];
+    const updatedEditing = { index, payload };
+
+    setEdit(updatedEditing);
+    console.log(updatedEditing);
+  }
+
+  function onItemRemoveHandler(itemId) {
+    const cartItems = [...cart.items];
+    const foundIndex = cart.items.findIndex((each) => each._id === itemId);
+
+    cartItems.splice(foundIndex, 1);
+    setCart((prev) => ({ ...prev, items: cartItems }));
+  }
 
   useEffect(() => {
     if (action === 'REPLACE' || action === 'POP') push('/sacola');
@@ -18,15 +38,23 @@ function CheckoutPage() {
 
   return (
     <Layout>
-      {cart.items.length ? (
-        <S.CheckoutItemsList>
-          {cart.items.map((each) => (
-            <CheckoutItem key={each._id} {...each} />
-          ))}
-        </S.CheckoutItemsList>
-      ) : (
-        <EmptyCheckoutMessage />
-      )}
+      <S.CheckoutItemsWrapper>
+        <S.CheckoutItemsTitle>Sua Sacola</S.CheckoutItemsTitle>
+        {cart.items.length ? (
+          <S.CheckoutItemsList>
+            {cart.items.map((each) => (
+              <CheckoutItem
+                key={each._id}
+                {...each}
+                onEdit={() => onItemEditHandler(each._id)}
+                onRemove={() => onItemRemoveHandler(each._id)}
+              />
+            ))}
+          </S.CheckoutItemsList>
+        ) : (
+          <EmptyCheckoutMessage />
+        )}
+      </S.CheckoutItemsWrapper>
     </Layout>
   );
 }
--- src/pages/Checkout/styled.js
+++ src/pages/Checkout/styled.js
@@ -2,9 +2,22 @@ import styled from 'styled-components';
 
 import { breakpoint } from '../../styles';
 
-export const CheckoutItemsList = styled.ul`
+export const CheckoutItemsWrapper = styled.div`
   @media screen and (min-width: ${breakpoint('md')}) {
     margin: 1.6rem 0 0 auto;
     max-width: 40rem;
   }
 `;
+
+export const CheckoutItemsTitle = styled.h2`
+  color: ${({ theme }) => theme.hexColors.lightest};
+  font-size: 2.4rem;
+  font-weight: 500;
+  margin: 0 0 3.2rem 0;
+
+  @media screen and (max-width: 1023px) {
+    display: none;
+  }
+`;
+
+export const CheckoutItemsList = styled.ul``;
