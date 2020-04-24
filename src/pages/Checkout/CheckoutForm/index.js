1ea33178e520e08d6beb93b6df0c4e7b81e997aa
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-24 14:50:55 -0300

    feat(checkout-form): add form change event, validate props
    - create event to call another form
    - validate payload when form change
    - pass event onNext and onPrev to step form

--- src/pages/Checkout/CheckoutForm/index.js
+++ src/pages/Checkout/CheckoutForm/index.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 import { steps } from './content';
 import * as S from './styled';
 
 function CheckoutForm() {
   const [active, setActive] = useState(steps[1]);
-  const [, setPayload] = useState({});
+  const [payload, setPayload] = useState({});
 
   function onActiveChange(activeForm) {
     setActive(activeForm);
   }
 
+  function onFormChange(type = 'next') {
+    if (type === 'next') {
+      const payloadKeys = Object.keys(payload);
+      const hasAbsentProp = active.requestedKeys
+        .map((key) => payloadKeys.includes(key) && payload[key])
+        .includes(false);
+
+      if (hasAbsentProp) {
+        toast.error('Você ainda não completou esta etapa!');
+        return;
+      }
+    }
+    setActive(steps[active[type]]);
+  }
+
   function onChangeHandler({ target }) {
     const { name, value } = target;
+
     return setPayload((prev) => ({ ...prev, [name]: value }));
   }
 
   function onSubmitHandler(e) {
     e.preventDefault();
+    console.log(payload);
   }
 
   return (
@@ -34,7 +52,11 @@ function CheckoutForm() {
         <S.CheckoutFormIndicator className={`--${active.targetClass}`} />
       </S.CheckoutFormSteps>
       <S.CheckoutFormContainer method="POST" onSubmit={onSubmitHandler}>
-        <active.component onChange={onChangeHandler} />
+        <active.component
+          onChange={onChangeHandler}
+          onNext={() => onFormChange('next')}
+          onPrev={() => onFormChange('prev')}
+        />
       </S.CheckoutFormContainer>
     </S.CheckoutFormWrapper>
   );
