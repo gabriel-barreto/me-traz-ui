4657755135f8070175560ae14ba615649ef7d99c
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-24 14:49:26 -0300

    feat(checkout-form): add link between steps on next and prev prop

--- src/pages/Checkout/CheckoutForm/content.js
+++ src/pages/Checkout/CheckoutForm/content.js
@@ -4,13 +4,21 @@ export const steps = {
   1: {
     targetClass: 'one',
     component: Forms.DeliveryCheckoutForm,
+    next: 2,
+    prev: null,
+    requestedKeys: ['deliveryType'],
   },
   2: {
     targetClass: 'two',
     component: Forms.UserCheckoutForm,
+    prev: 1,
+    next: 3,
+    requestedKeys: ['name', 'email', 'whatsapp'],
   },
   3: {
     targetClass: 'three',
     component: Forms.PaymentCheckoutForm,
+    prev: 2,
+    requestedKeys: ['paymentType'],
   },
 };
