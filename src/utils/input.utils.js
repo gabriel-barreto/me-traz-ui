f3893e07c97e9a2dd17c4d1ed0be3fb1ef7c46b9
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-08 15:20:32 -0300

    feat(input-masks): add a simple money mask

--- src/utils/input.utils.js
+++ src/utils/input.utils.js
@@ -18,7 +18,14 @@ function cepMask(value) {
     .replace(/(\d{3})(\d)/, '$1-$2');
 }
 
+function moneyMask(value) {
+  if (!value) return value;
+
+  return value.replace(/\D/g, '').replace(/(\d+?$)/, 'R$ $1');
+}
+
 export const masks = {
   cep: cepMask,
+  money: moneyMask,
   phone: phoneMask,
 };
