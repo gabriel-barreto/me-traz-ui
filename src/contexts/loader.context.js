702540b27023fdac57a2c8eb22987230a0dfaf19
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-15 17:49:16 -0300

    feat(loader-context): add observer to loader context
    add pubsub subscription to loader context to can update state outside a component

--- src/contexts/loader.context.js
+++ src/contexts/loader.context.js
@@ -1,16 +1,19 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import PubSub from 'pubsub-js';
 
+const CONTEXT_TOPIC = 'loaderContext';
 const INITIAL_STATE = { visible: false, label: 'Carregando...' };
-const LoaderContext = createContext({});
-const state = { ...INITIAL_STATE };
-
-export const setLoader = (payload = INITIAL_STATE) =>
-  Object.assign(state, payload);
+const LoaderContext = createContext(INITIAL_STATE);
 
 export function LoaderProvider({ children }) {
-  const [config, setConfig] = useState(state);
+  const [config, setConfig] = useState(INITIAL_STATE);
+  const subscriber = (_, newConfig) =>
+    setConfig((prev) => ({ ...prev, ...newConfig }));
 
+  useEffect(() => {
+    PubSub.subscribe(CONTEXT_TOPIC, subscriber);
+  });
   return (
     <LoaderContext.Provider value={{ config, setConfig }}>
       {children}
@@ -21,6 +24,7 @@ LoaderProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
+export const loaderContextTopic = CONTEXT_TOPIC;
 export function useLoader() {
   const { config, setConfig } = useContext(LoaderContext);
   return { config, setConfig };
